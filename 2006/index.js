1032d372f52fa9838e1af05d079b15a72f4119ca
MartiniGlauber <gmartinisantos@gmail.com>
2024-10-16 17:56:29 -0300

    iniciando exercicio 2006

---
+++ 2006/index.js
@@ -0,0 +1,17 @@
+let input = require("fs").readFileSync("dev/stdin", "utf-8");
+let lines = input.split("\n");
+
+const teaFlavor = parseInt(lines[0]);
+let answer = lines[1].split(' ').map((x) => parseInt(x));
+let rightAnswer = 0;
+
+function verifyAnswer() {
+    for (let i = 0; i < answer.length; i++) {
+        if(answer[i] == teaFlavor){
+            rightAnswer++;
+        }
+    }
+    return console.log(rightAnswer);
+}
+
+verifyAnswer(lines);
